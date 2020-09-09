4a5959930a873f154274d0d57a34f4ac11a30839
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-09 15:38:59 -0500

    created useEffect function to pull data from API on app mount

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import Photo from "./Components/Photo";
+import { BASE_URL, API_KEY } from "./constants/const";
 
 function App() {
-  const [photoURL, setPhotoURL] = useState();
+  const [photoData, setPhotoData] = useState("");
+  const [photoURL, setPhotoURL] = useState("");
 
-  function getPhoto(date) {}
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then((res) => {
+        setPhotoURL(res.data.url);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="App">
-      <div className="photo-container"></div>
+      <div className="photo-container">{/* <Photo url={photoURL} /> */}</div>
     </div>
   );
 }
---
+++ src/constants/const.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://api.nasa.gov/planetary/apod/";
+export const API_KEY = "Zzs3yK2IbvNp8ceZq8KTfdaH6y9ZAtoTp8J3ADr6";
